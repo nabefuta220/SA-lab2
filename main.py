9efea843e4440ddb91f78997e5f874234fd60523
nabefuta <satoshinabe@220@gmail.com>
2021-12-02 15:55:40 +0900

    created pca

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 """
 プログラム実行時の根幹部分
 """
-
-from src.read_data import read_file
 import pandas as pd
+from src.pca import pca
+from src.read_data import read_file
 
 if __name__ == "__main__":
+    # ファイルの読み込み
     res = read_file('output/seiseki.txt', 'shift-jis')
-    res.pop()
     data = pd.DataFrame(res[1:], columns=res[0])
-    print(data)
+    # 欠損値が含まれる行を削除
+    data = data.dropna(axis=0).astype('float64')
+    print(f"data:\n{data.head()}")
+    
+    pca(data, 'score')
