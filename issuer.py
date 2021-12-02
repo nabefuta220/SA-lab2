d545fbc4614fb2180c218939614dcc5c318ac78f
nabefuta <satoshinabe@220@gmail.com>
2021-12-02 11:57:25 +0900

    add docstring

--- issuer.py
+++ issuer.py
@@ -1,9 +1,16 @@
+"""
+ファイルを実行し、標準出力をファイルで返す
+"""
+
 import argparse
 import os
 import subprocess
 
 
 def inputer():
+    """
+    入力時のパーサー
+    """
     arg = argparse.ArgumentParser()
     arg.add_argument('command', nargs='*')
     parse = arg.parse_args()
@@ -11,11 +18,11 @@ def inputer():
 
 
 if __name__ == '__main__':
-    str = inputer()
-    print(str)
-    out_file = f"output/{os.path.splitext(os.path.basename(str[1]))[0]}.txt"
+    command_line = inputer()
+    print(command_line)
+    out_file = f"output/{os.path.splitext(os.path.basename(command_line[1]))[0]}.txt"
     print(out_file)
-    command = ['pipenv', 'run']+str
+    command = ['pipenv', 'run']+command_line
     cp = subprocess.run(command, encoding='utf-8',
                         stdout=subprocess.PIPE, check=True)
 
