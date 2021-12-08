68c47c8b5f79cfcf46c897374a8aa5e69fa6a5af
nabefuta <satoshinabe@220@gmail.com>
2021-12-08 17:50:32 +0900

    pokemonの主成分分析

--- pokemon-analize.py
+++ pokemon-analize.py
@@ -8,7 +8,7 @@ from src.read_data import read_file
 
 if __name__ == "__main__":
     # ファイルの読み込み
-    res = read_file('output/pokemon.txt', 'UTF-8')
+    res = read_file('output/pokemon.txt', 'shift-jis')
     data = pd.DataFrame(res[1:], columns=res[0])
     #data = data.drop(columns=['選手', 'チーム', '打席', '試合', '打数', '塁打'], axis=1)
     # 欠損値が含まれる行を削除
